ae5e67c3e6e6d7edc573be8b55261b33f22a77e0
工作 <licheng6256@tutamail.com>
2026-03-21 03:54:29 +0800

    增加迁移包

--- config/log.php
+++ config/log.php
@@ -26,7 +26,17 @@ return [
                     'class' => Monolog\Formatter\LineFormatter::class,
                     'constructor' => [null, 'Y-m-d H:i:s', true],
                 ],
-            ]
+            ],
+            // Telegram 错误通知
+            [
+                'class' => app\handler\TelegramHandler::class,
+                'constructor' => [
+                    env('TELEGRAM_BOT_TOKEN', ''),
+                    env('TELEGRAM_CHAT_ID', ''),
+                    env('TELEGRAM_APP_NAME', 'Webman App'),
+                    Monolog\Logger::ERROR, // 只发送 ERROR 及以上级别的日志
+                ],
+            ],
         ],
     ],
     'machine' => [
